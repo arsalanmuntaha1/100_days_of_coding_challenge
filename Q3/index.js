adee2df459e88d36bb6b4edbb149932f9ddf4030
arsalanmuntaha1 <arsalanmuntaha1@gmail.com>
2024-03-11 03:08:19 +0500

    Q3 is done

---
+++ Q3/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// lower case
+let personName = "mUntaHa";
+console.log(personName.toLowerCase());
+// UPPER CASE
+console.log(personName.toUpperCase());
+//Title Case
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
---
+++ Q3/index.ts
@@ -0,0 +1,14 @@
+// lower case
+
+let personName = "mUntaHa"; 
+console.log(personName.toLowerCase());                                                                                   
+
+
+// UPPER CASE
+console.log(personName.toUpperCase());
+
+
+//Title Case
+
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase())
+
