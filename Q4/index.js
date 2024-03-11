89cdd0694e0780c4b528437af365636780d5a58a
arsalanmuntaha1 <arsalanmuntaha1@gmail.com>
2024-03-12 00:22:59 +0500

    Q4 is done

---
+++ Q4/index.js
@@ -0,0 +1,3 @@
+"use strict";
+let variable = 'Oscar Wilde said: "Always forgive your enemies; nothing annoys them so much."';
+console.log(variable);
---
+++ Q4/index.ts
@@ -0,0 +1,2 @@
+let variable = 'Oscar Wilde said: "Always forgive your enemies; nothing annoys them so much."';
+console.log(variable)
