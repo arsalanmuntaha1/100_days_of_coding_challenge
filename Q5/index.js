07283561e0ddd30ce97f2bf4272727e4e6fd721b
arsalanmuntaha1 <arsalanmuntaha1@gmail.com>
2024-03-13 20:40:26 +0500

    Q5 updated

---
+++ Q5/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let personName = "Oscar Wilde";
+let message = `${personName} said: "Always forgive your enemies; nothing annoys them so much."`;
+console.log(message);
---
+++ Q5/index.ts
@@ -0,0 +1,3 @@
+let personName : string = "Oscar Wilde";
+let message : string = `${personName} said: "Always forgive your enemies; nothing annoys them so much."`;
+console.log(message);
