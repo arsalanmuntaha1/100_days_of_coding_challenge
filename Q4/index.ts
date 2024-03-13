0ac2db62cd9bff6e212f0821e32d4e8a099466aa
arsalanmuntaha1 <arsalanmuntaha1@gmail.com>
2024-03-13 20:39:52 +0500

    Q4 updated

--- Q4/index.ts
+++ Q4/index.ts
@@ -1,2 +1,2 @@
-let variable = 'Oscar Wilde said: "Always forgive your enemies; nothing annoys them so much."';
+let variable : string = 'Oscar Wilde said: "Always forgive your enemies; nothing annoys them so much."';
 console.log(variable)
