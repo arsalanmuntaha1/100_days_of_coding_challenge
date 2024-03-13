6fd3800c839e2b3d68adf231269fe80e0634a267
arsalanmuntaha1 <arsalanmuntaha1@gmail.com>
2024-03-13 22:59:30 +0500

    Q7 is done

---
+++ Q7/index.js
@@ -0,0 +1,13 @@
+"use strict";
+//Addition
+let variable = 4 + 4;
+console.log(variable);
+//Subtraction
+variable = 12 - 4;
+console.log(variable);
+// Multiplication
+variable = 2 * 2 * 2;
+console.log(variable);
+// Division
+variable = 40 / 5;
+console.log(variable);
